e3a66d9f8f866f27636bfb3fe92198643f99a3c6
SongeunB <bksoon0427@gmail.com>
2022-01-24 09:09:42 +0900

    home

--- final_project/src/main/java/com/jhta/finalproject/controller/HomeController3.java
+++ final_project/src/main/java/com/jhta/finalproject/controller/HomeController3.java
@@ -9,13 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HomeController2 {
+public class HomeController3 {
 	@Autowired ServletContext sc;
 	
-	@RequestMapping(value = "/home/home", method = RequestMethod.GET)
-	public String home(Model model) {
+	@RequestMapping(value = "/home/home2", method = RequestMethod.GET)
+	public String home() {
 		sc.setAttribute("cp",sc.getContextPath());
-		return "home/home";
+		/*
+		 * model.addAttribute("header", sc.getContextPath()+"/home/header.jsp");
+		 * model.addAttribute("main", sc.getContextPath()+"/home/home2.jsp");
+		 * model.addAttribute("footer", sc.getContextPath()+"/home/footer.jsp");
+		 */
+		return "home/home2";
 	}
 	
 }
