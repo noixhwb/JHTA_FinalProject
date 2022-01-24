6463af2dc7a46bd67c8ad84b925c3e22c0fac2bc
SongeunB <bksoon0427@gmail.com>
2022-01-25 01:04:33 +0900

    main추가, home-header-footer 연결, 제이쿼리 추가

---
+++ final_project/src/main/java/com/jhta/finalproject/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.jhta.finalproject.controller;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	@Autowired ServletContext sc;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		sc.setAttribute("cp",sc.getContextPath());
+		return "main";
+	}
+	
+}
