faf02d8558245aac7a9a78cc3c73ee331057e6f0
Seunghan <Seunghan@DESKTOP-2M4CH25>
2022-01-24 17:44:46 +0900

    흐아

--- final_project/src/main/java/com/jhta/finalproject/jobcontroller/JobInsert.java
+++ final_project/src/main/java/com/jhta/finalproject/jobcontroller/JobInsert.java
@@ -1,17 +1,27 @@
 package com.jhta.finalproject.jobcontroller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.UUID;
+
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.jhta.finalproject.service.JobService;
 import com.jhta.finalproject.vo.JobVo;
 
 @Controller
 public class JobInsert {
+	@Autowired private ServletContext sc;
 	@Autowired private JobService service;
 	
 	@GetMapping("/job/insertForm")
@@ -20,9 +30,25 @@ public class JobInsert {
 	}
 	
 	@PostMapping("/job/insert")
-	public String insert(JobVo vo,Model model) {
-		
-		service.insert(vo);
+	public String insert(JobVo vo,MultipartFile file1,Model model) {
+		String path=sc.getRealPath("/resources/upload");
+		String orgfilname=file1.getOriginalFilename();
+		String savefilename=UUID.randomUUID() + "_" + orgfilname;
+		try {
+			InputStream is=file1.getInputStream();
+			File f=new File(path + "\\" + savefilename);
+			FileOutputStream fos=new FileOutputStream(path + "\\" + savefilename);
+			FileCopyUtils.copy(is, fos);
+			long filesize=file1.getSize();
+			is.close();
+			fos.close();
+			service.insert(vo);
+//			service.insert(new FileInfoVo(0, writer, title, content, orgfilename, savefilename, filesize));
+			model.addAttribute("result","fail");
+		}catch(Exception e) {
+			e.printStackTrace();
+			model.addAttribute("result","fail");
+		}
 		return "job/result";
 	}
 }
--- final_project/src/main/webapp/WEB-INF/views/job/insertForm.jsp
+++ final_project/src/main/webapp/WEB-INF/views/job/insertForm.jsp
@@ -13,13 +13,16 @@
 	공고명 <input type="text" id="j_subject" name="j_subject"><br>
 	채용시작날짜 <input type="date" id="j_startdate" name="j_startdate"><br>
 	채용마감날짜 <input type="date" id="j_enddate" name="j_enddate"><br>
-	직무 <input type="text" id="j_duty" name="j_duty"><br>
-	지역 <input type="text" id="j_region" name="j_region"><br>
-	경력 <input type="text" id="j_career" name="j_career"><br>
+	
+	직무* <input type="text" id="j_duty" name="j_duty"><br>
+	지역* <input type="text" id="j_region" name="j_region"><br>
+	경력* <input type="text" id="j_career" name="j_career"><br>
+	
 	공고URL <input type="text" id="j_rul" name="j_rul"><br>
-	기업사진 <input type="text" id="j_img" name="j_img"><br>
-	첨부파일<br>
+	기업이미지 첨부<br>
 	<input type="file" name="file1"><br>
+	공고이미지 첨부<br>
+	<input type="file" name="file2"><br>
 	<input type="submit" value="신청하기">
 </form>
 </body>
