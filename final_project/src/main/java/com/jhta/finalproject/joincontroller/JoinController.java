a84431819f0bf96823bf2bbaec0f154e79615f1a
lamgu <lamgu@lamgul>
2022-01-24 15:42:38 +0900

    1

--- final_project/src/main/java/com/jhta/finalproject/joincontroller/JoinController.java
+++ final_project/src/main/java/com/jhta/finalproject/joincontroller/JoinController.java
@@ -1,4 +1,4 @@
-package com.jhta.fianlproject.joincontroller;
+package com.jhta.finalproject.joincontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
