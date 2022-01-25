1dd7835a7621c1376f2957c5f9f9ccf20eca5ad1
lamgu <lamgu@lamgul>
2022-01-25 14:40:30 +0900

    1

--- final_project/src/main/java/com/jhta/finalproject/dealcontroller/DealInsertController.java
+++ final_project/src/main/java/com/jhta/finalproject/dealcontroller/DealInsertController.java
@@ -3,21 +3,14 @@ package com.jhta.finalproject.dealcontroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.jhta.finalproject.service.DealService;
-import com.jhta.finalproject.vo.DealVo;
 
 @Controller
-public class DealInsert {
+public class DealInsertController {
 	@Autowired private DealService service;
 	@GetMapping("/deal/dealinsert")
 	public String insertForm() {
 		return "deal/dealinsert";
-	}
-	@PostMapping("/deal/dealinsert")
-	public String insert(DealVo vo) {
-		service.insert(vo);
-		return "redirect:deal/deallist";
 	}
 }
