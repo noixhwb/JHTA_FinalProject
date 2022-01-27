a47bd867218a6f2f8332860a992b74227956c573
lamgu <lamgu@192.168.219.108>
2022-01-27 17:30:52 +0900

    1

--- final_project/src/main/java/com/jhta/finalproject/dealcontroller/DealList.java
+++ final_project/src/main/java/com/jhta/finalproject/dealcontroller/DealList.java
@@ -1,5 +1,6 @@
 package com.jhta.finalproject.dealcontroller;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.jhta.finalproject.service.DealService;
 import com.jhta.finalproject.vo.DealVo;
+import com.jhta.finalproject.vo.Goods_ImageVo;
 import com.util.PageUtil;
 
 @Controller
@@ -22,15 +24,20 @@ public class DealList {
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("word", word);
 		int totalRowCount = service.count(word);
-		PageUtil pu = new PageUtil(pageNum, 9,5,totalRowCount);
+		PageUtil pu = new PageUtil(pageNum, 6,5,totalRowCount);
 		int startRow = pu.getStartRow();//시작행번호
 		int endRow = pu.getEndRow();//끝행번호
 		
 		map.put("startRow", startRow);
 		map.put("endRow",endRow);
-
+		List<Goods_ImageVo> imglist = Collections.EMPTY_LIST;
 		List<DealVo> list =service.selectAll(map);
+		for(DealVo li:list) {
+			List<Goods_ImageVo> img = service.selectImg(li.getT_num());
+			imglist.add(img.get(0));
+		}
 		model.addAttribute("list", list);
+		model.addAttribute("imglist", imglist);
 		model.addAttribute("pu", pu);
 		model.addAttribute("keyword",word);
 		return "deal/dealist";
